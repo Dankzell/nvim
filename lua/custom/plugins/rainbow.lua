f3a37913d131109a1f61c8901f2735d037650436
gustav forsell <gustav.forsell@br-automation.com>
2025-10-09 19:43:39 +0200

    add rainbow

--- lua/custom/plugins/rainbow.lua
+++ lua/custom/plugins/rainbow.lua
@@ -2,6 +2,16 @@ return {
   'HiPhish/rainbow-delimiters.nvim',
   event = { 'BufReadPost', 'BufNewFile' },
   config = function()
+    local colors = require('catppuccin.palettes').get_palette 'frappe'
+
+    vim.api.nvim_set_hl(0, 'RainbowDelimiterRed', { fg = colors.red })
+    vim.api.nvim_set_hl(0, 'RainbowDelimiterYellow', { fg = colors.yellow })
+    vim.api.nvim_set_hl(0, 'RainbowDelimiterBlue', { fg = colors.blue })
+    vim.api.nvim_set_hl(0, 'RainbowDelimiterOrange', { fg = colors.peach })
+    vim.api.nvim_set_hl(0, 'RainbowDelimiterGreen', { fg = colors.green })
+    vim.api.nvim_set_hl(0, 'RainbowDelimiterViolet', { fg = colors.mauve })
+    vim.api.nvim_set_hl(0, 'RainbowDelimiterCyan', { fg = colors.teal })
+
     vim.g.rainbow_delimiters = {
       strategy = {
         [''] = 'rainbow-delimiters.strategy.global',
@@ -25,6 +35,7 @@ return {
         'RainbowDelimiterCyan',
       },
     }
+
     require('rainbow-delimiters.setup').setup()
   end,
 }
